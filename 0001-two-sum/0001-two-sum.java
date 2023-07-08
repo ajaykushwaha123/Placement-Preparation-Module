a214e6cc86d671e4f266bc61d291f394eb4eee0f
Ajay kushwaha <85159512+ajaykushwaha123@users.noreply.github.com>
2023-07-08 19:04:14 +0530

    Time: 4 ms (63.15%), Space: 43.5 MB (77.04%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer,Integer>map=new HashMap();
+        
         for(int i=0;i<nums.length;i++){
             map.put(nums[i],i);
         }
@@ -15,5 +16,6 @@ class Solution {
             }
         }
         return new int[]{};
+        
     }
 }
