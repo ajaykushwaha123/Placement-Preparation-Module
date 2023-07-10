ee91e252d2ed9cb2c263622ecbcbce21ca46c7b9
Ajay kushwaha <85159512+ajaykushwaha123@users.noreply.github.com>
2023-07-10 11:27:51 +0530

    Time: 4 ms (86.23%), Space: 43.6 MB (45.28%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -5,5 +5,24 @@ class Solution {
         Arrays.sort(a1);
         Arrays.sort(a2);
         return Arrays.equals(a1,a2);
+//         HashMap<Character,Integer> map = new HashMap<>();
+//         for( int i = 0; i < s.length() ; i++){
+//             if( map.containsKey(s.charAt(i))){
+//             map.put(s.charAt(i) , map.get(s.charAt(i)) + 1);
+//         }
+          
+//     }
+//                 for( int i = 0; i < t.length() ; i++){
+//             if( map.containsKey(t.charAt(i))){
+//             map.put(s.charAt(i) , map.get(s.charAt(i)) - 1);
+//             }
+//                for( int val : map.values()){
+//                    if(val != 0){
+//                        return false;
+//                    }
+//                }
+               
+// }
+//         return true;
     }
 }
