df4791163f4ad36fc165b2378b78eb88689dff56
Ajay kushwaha <85159512+ajaykushwaha123@users.noreply.github.com>
2023-08-07 21:04:00 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (98.40%) - LeetHub

---
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -0,0 +1,20 @@
+class Solution {
+   public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        int n = rooms.size();
+        boolean[] visited = new boolean[n];
+        int ans = helper(rooms, visited, 0,n);
+        return ans == n;
+    }
+    public int helper(List<List<Integer>> rooms, boolean[] visited, int curr, int n) {
+        if(visited[curr]) {
+            return 0;
+        }
+        visited[curr] = true;
+        int ans = 1;
+        for(int i = 0; i<rooms.get(curr).size(); i++) {
+            List<Integer> keys = rooms.get(curr);
+            ans += helper(rooms, visited, keys.get(i), n);
+        }
+        return ans;
+    }
+}
