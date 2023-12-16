b3f175df244bd8de4386642eb2c8e60b82cd1fb4
Ajay kushwaha <85159512+ajaykushwaha123@users.noreply.github.com>
2023-12-16 13:20:42 +0530

    Time: 1 ms (99.86%), Space: 43.6 MB (48.93%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,28 +1,18 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        char a1[] = s.toCharArray();
-        char a2[] = t.toCharArray();
-        Arrays.sort(a1);
-        Arrays.sort(a2);
-        return Arrays.equals(a1,a2);
-//         HashMap<Character,Integer> map = new HashMap<>();
-//         for( int i = 0; i < s.length() ; i++){
-//             if( map.containsKey(s.charAt(i))){
-//             map.put(s.charAt(i) , map.get(s.charAt(i)) + 1);
-//         }
-          
-//     }
-//                 for( int i = 0; i < t.length() ; i++){
-//             if( map.containsKey(t.charAt(i))){
-//             map.put(s.charAt(i) , map.get(s.charAt(i)) - 1);
-//             }
-//                for( int val : map.values()){
-//                    if(val != 0){
-//                        return false;
-//                    }
-//                }
-               
-// }
-//         return true;
+       int alphas[] = new int[26];
+        for(char ch : s.toCharArray()){
+            alphas[ch-'a']++;
+        }
+        for(char ch : t.toCharArray()){
+            alphas[ch-'a']--;
+        }
+        for(int i : alphas){
+            if(i != 0){
+                return false;
+            }
+        }
+        return true;
+        
     }
 }
