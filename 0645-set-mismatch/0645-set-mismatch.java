9b91d893982802416e588359463fcfd2d1995b39
Ajay Kushwaha <85159512+ajaykushwaha123@users.noreply.github.com>
2024-01-22 15:55:56 +0530

    Time: 13 ms (14.85%), Space: 45.9 MB (8.03%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+       int ans[] = new int[2] ;
+        int n = nums.length ;
+        HashSet<Integer> set = new HashSet<>() ;
+        int sum = n*(n+1)/2 ;
+        int repeat = -1 ;
+        for(int val : nums){
+            if(set.contains(val)){
+                repeat = val ;
+            }
+            else{
+                set.add(val) ;
+                sum -= val ;
+            }
+        }
+        ans[0] = repeat ;
+        ans[1] = sum ;
+        return ans ;
+    }
+}
