3057acad0efc4b6245d406ab446c09bf769ec952
Ajay Kushwaha <85159512+ajaykushwaha123@users.noreply.github.com>
2024-01-29 15:45:37 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (10.51%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -4,20 +4,13 @@ class MyQueue {
     public void push(int x) {
         Stack<Integer> tmpValues = new Stack<>();
 
-        //read all stored elements and put all in new stack in reverse order:
-        // a) read from top to tail (where on top stored newest elements)
-        // b) write all values in new order (where on top stored oldest elements)
-        // * delete all readed values from stack
+        
         while (!this.values.isEmpty()) {
             tmpValues.push(values.pop());
         }
 
-        // add new value on the top on a stack (it will ne first value on stored stack)
+        
         this.values.push(x);
-
-        // Move all elements back from the tmp stack to the stored, where:
-        // a) new value x will be on the tail of stored values
-        // b) all stored values will store like FIFO
         while (!tmpValues.isEmpty()) {
             this.values.push(tmpValues.pop());
         }
