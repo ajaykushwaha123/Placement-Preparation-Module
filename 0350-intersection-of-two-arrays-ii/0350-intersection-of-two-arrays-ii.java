aa0b15ddb9b2dfccbce7da2f76a15fae26c57ce1
Ajay Kushwaha <85159512+ajaykushwaha123@users.noreply.github.com>
2024-07-02 15:39:21 +0530

    Time: 3 ms (90.95%), Space: 43 MB (58.37%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,26 +1,25 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        	ArrayList<Integer> ans = new ArrayList<>();
-		Map<Integer, Integer> freq = new HashMap<>();
-
-		for(int i=0;i<nums1.length;i++) {
-			if(!freq.containsKey(nums1[i]))
-				freq.put(nums1[i], 1);
-			else
-				freq.put(nums1[i], freq.get(nums1[i]) + 1);
-		}
-		for(int i=0;i<nums2.length;i++) {
-			if(freq.containsKey(nums2[i]) && freq.get(nums2[i]) > 0) {
-				freq.put(nums2[i], freq.get(nums2[i]) - 1);
-				ans.add(nums2[i]);
-			}
-		}
-		int n = ans.size();
-		int[] res = new int[n];
-		for(int i=0;i<n;i++) {
-			res[i] = ans.get(i);
-		}
-		return res;
-        
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i=0,j=0;
+        ArrayList<Integer> list = new ArrayList<>();
+        while(i<nums1.length && j<nums2.length){
+            if(nums1[i]==nums2[j]){
+                list.add(nums1[i]);
+                i++;j++;
+            }
+            else if(nums1[i]<nums2[j]){
+                i++;
+            }
+            else{
+                j++;
+            }
+        }
+        int[] res = new int[list.size()];
+        for(i=0;i<list.size();i++){
+            res[i]=list.get(i);
+        }
+        return res;
     }
 }
