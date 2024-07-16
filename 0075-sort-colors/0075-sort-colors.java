e482e511268b60cbe86039fc54cad9fb406f50af
Ajay Kushwaha <85159512+ajaykushwaha123@users.noreply.github.com>
2024-07-16 11:47:05 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (38.40%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,14 +1,41 @@
 class Solution {
     public void sortColors(int[] nums) {
-        // insertion sort
-        for(int i = 1; i< nums.length; i++){
-            int j = i-1;
-            int temp = nums[i];
-            while(j >=0 && nums[j]>temp){
-                nums[j+1] = nums[j];
-                j--;
+        int zero = 0;
+        int one = 0;
+        int two = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == 0){
+                zero++;
             }
-            nums[j+1] = temp;
+            else if(nums[i] == 1){
+                one++;
+            }
+            else{
+                two++;
+            }
         }
+        int a = 0;
+        for(int i = 1; i <= zero; i++){
+            nums[a] = 0;
+            a++;
+        }
+        for(int i = 1; i <= one; i++){
+            nums[a] = 1;
+            a++;
+        }
+        for(int i = 1; i <= two; i++){
+            nums[a] = 2;
+            a++;
+        }
+        // // insertion sort
+        // for(int i = 1; i< nums.length; i++){
+        //     int j = i-1;
+        //     int temp = nums[i];
+        //     while(j >=0 && nums[j]>temp){
+        //         nums[j+1] = nums[j];
+        //         j--;
+        //     }
+        //     nums[j+1] = temp;
+        // }
     }
 }
