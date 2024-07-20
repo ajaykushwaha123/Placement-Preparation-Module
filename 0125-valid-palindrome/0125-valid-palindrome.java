5003617d6542f759510cf7fbf2f91dd89bb33f11
Ajay Kushwaha <85159512+ajaykushwaha123@users.noreply.github.com>
2024-07-20 15:05:05 +0530

    Time: 5 ms (46.48%), Space: 43.1 MB (57.26%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -2,16 +2,18 @@ class Solution {
     public boolean isPalindrome(String s) {
         s = s.toLowerCase();
         StringBuilder sb = new StringBuilder();
-        for(int i =0; i< s.length(); i++){
+        for(int i = 0; i < s.length(); i++){
             if(Character.isDigit(s.charAt(i)) || Character.isLetter(s.charAt(i))){
                 sb.append(s.charAt(i));
             }
         }
-        int n = sb.length() -1;
-        for(int i = 0; i<sb.length()/2; i++){
-            if(sb.charAt(i) != sb.charAt(n-i))
+        int n = sb.length()-1;
+        for(int i = 0; i < sb.length()/2; i++){
+            if(sb.charAt(i) != sb.charAt(n-i)){
                 return false;
+            }
         }
         return true;
+        
     }
 }
