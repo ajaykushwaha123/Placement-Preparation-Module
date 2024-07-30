a4c5cd6180cc464aa50c76e52e5423ff703c42a0
Ajay Kushwaha <85159512+ajaykushwaha123@users.noreply.github.com>
2024-07-30 12:34:00 +0530

    Time: 15 ms (15.03%), Space: 56.7 MB (84.05%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -4,7 +4,7 @@ class Solution {
         int a=1;
     for(int i=0;i<nums.length;i++)
     {
-        if(nums[i]>0 & a==nums[i])
+        if(nums[i]>0 && a==nums[i])
         {
          a++;
         }
