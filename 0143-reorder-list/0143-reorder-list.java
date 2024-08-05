b3a673f1250a8d54845201fadd477a38dafe0b8f
Ajay Kushwaha <85159512+ajaykushwaha123@users.noreply.github.com>
2024-08-05 16:09:13 +0530

    Time: 4 ms (16.27%), Space: 47.4 MB (96.22%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -11,20 +11,20 @@
 class Solution {
     public void reorderList(ListNode head) {
         Deque<ListNode> deque = new LinkedList<>();
-        ListNode temp=head.next;
-        while(temp!=null){
+        ListNode temp = head.next;
+        while(temp != null){
             deque.offer(temp);
-            temp=temp.next;
+            temp = temp.next;
         }
-        temp=head;
+        temp = head;
         while(!deque.isEmpty()){
-            temp.next=deque.pollLast();
-            temp=temp.next;
+            temp.next = deque.pollLast();
+            temp = temp.next;
             if(!deque.isEmpty()){
-                temp.next=deque.pollFirst();
-                temp=temp.next;
+                temp.next = deque.pollFirst();
+                temp = temp.next;
             }
         }
-        temp.next=null;
+        temp.next = null;
     }
 }
