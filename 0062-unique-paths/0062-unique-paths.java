b8777cb0b076a956b63122b73d5daf68f77ffaa4
Ajay Kushwaha <85159512+ajaykushwaha123@users.noreply.github.com>
2024-08-09 12:26:56 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (56.13%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -4,10 +4,10 @@ class Solution {
         for(int i = 0; i < dp.length; i++){
             dp[i][0] = 1;
         }
-        for(int i =0; i< dp[0].length; i++){
+        for(int i = 0; i < dp[0].length; i++){
             dp[0][i] = 1;
         }
-        for(int i =1; i < dp.length; i++ ){
+        for(int i = 1; i < dp.length; i++){
             for(int j = 1; j < dp[0].length; j++){
                 dp[i][j] = dp[i-1][j] + dp[i][j-1];
             }
