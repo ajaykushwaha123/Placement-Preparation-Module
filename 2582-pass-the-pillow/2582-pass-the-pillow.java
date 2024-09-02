f35ca6aba9f8e138a6e3e36e933eee446d54ea62
Ajay Kushwaha <85159512+ajaykushwaha123@users.noreply.github.com>
2024-09-02 14:18:38 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (34.38%) - LeetHub

---
+++ 2582-pass-the-pillow/2582-pass-the-pillow.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int passThePillow(int n, int time) {
+       int fr = time / (n-1);
+        int et = time%(n-1);
+        if(fr % 2 ==0){
+            return et+1;
+        }
+        return n -et;
+    }
+}
