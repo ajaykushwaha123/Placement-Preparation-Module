f6a1119994c49f8e162dcdd2d880a1eb49fbc12f
Ajay Kushwaha <85159512+ajaykushwaha123@users.noreply.github.com>
2024-09-05 12:09:03 +0530

    Time: 1 ms (27.23%), Space: 40.9 MB (25.16%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if(n==1){
+            return true;
+        }
+        for(int i = 1 ;i <= 32 ; i++){
+            if(Math.pow(4,i)==n){
+                return true;
+            }
+        }
+        return false;
+    }
+}
