e6970c7a871dda5dc9bb55436b44d911672d1da4
Ajay Kushwaha <85159512+ajaykushwaha123@users.noreply.github.com>
2024-10-05 12:27:42 +0530

    Time: 6 ms (86.36%), Space: 42.2 MB (97.31%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -3,23 +3,24 @@ class Solution {
         if (s1.length() > s2.length())
             return false;
         int[] s1arr = new int[26];
-        int[] s2arr = new int[26];
         for (int i = 0; i < s1.length(); i++) {
             s1arr[s1.charAt(i) - 'a']++;
-            s2arr[s2.charAt(i) - 'a']++;
         }
-        for (int i = 0; i < s2.length() - s1.length(); i++) {
-            if (matches(s1arr, s2arr))
+        for (int i = 0; i < s2.length(); i++) {
+           s1arr[s2.charAt(i) - 'a']--;
+            if(i - s1.length() >= 0)
+                s1arr[s2.charAt(i-s1.length()) - 'a']++;
+            if(matches(s1arr))
                 return true;
-            s2arr[s2.charAt(i + s1.length()) - 'a']++;
-            s2arr[s2.charAt(i) - 'a']--;
+                
+            
         }
-        return matches(s1arr, s2arr);
+        return false;
     }
     
-    public boolean matches(int[] s1arr, int[] s2arr) {
+    public boolean matches(int[] s1arr) {
         for (int i = 0; i < 26; i++) {
-            if (s1arr[i] != s2arr[i])
+            if (s1arr[i] != 0)
                 return false;
         }
         return true;
